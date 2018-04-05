354fd08f58ea4386f184ae129427b72e63982334
Sahana Balram <sahana2008balram@gmail.com>
2018-04-05 16:01:35 -0700

    Front end

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const multer = require("multer");
-const app = express();
+const app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
