9dbd263081704aaf1a4bafa4f4afda5f4a9f0e78
Sahana Balram <sahana2008balram@gmail.com>
2018-04-05 16:04:36 -0700

    Start the server

--- server.js
+++ server.js
@@ -4,5 +4,11 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const multer = require("multer");
 const app = module.exports = express();
+const PORT = process.env.port || 3000;
 app.use(bodyParser.json());
 app.use(cors());
+
+
+app.listen(PORT,function(){
+    console.log("App is listening on: " + PORT);
+});
