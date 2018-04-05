38986df26217776670df4bf9111f1bd0e0634b32
Sahana Balram <sahana2008balram@gmail.com>
2018-04-05 16:12:33 -0700

    Server side post method

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const cors = require("cors");
 const multer = require("multer");
 const app = module.exports = express();
 const PORT = process.env.port || 3000;
+const upload = multer({dest: "uploads/"});
 app.use(bodyParser.json());
 app.use(cors());
 
+app.post("/upload",upload.single("file"),function(req,res,next){
 
+});
 app.listen(PORT,function(){
     console.log("App is listening on: " + PORT);
 });
