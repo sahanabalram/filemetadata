e103e2c0229b0f24b66537144e572f4290236e7e
Sahana Balram <sahana2008balram@gmail.com>
2018-04-05 15:59:51 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+// Import the dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const multer = require("multer");
+const app = express();
